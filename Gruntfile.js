8f6ed015fdbe08443a2405a9a36bc151b1959b8e
Frank Heiss <heissmusik@gmail.com>
2014-10-05 13:21:02 -0400

    install and configure grunt concat and less

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,15 +32,66 @@ module.exports = function(grunt) {
       options: {
         livereload: true
       },
-    }
+    },
+
+    // TODO: use require.js already
+    concat: {
+      options: {
+        separator: ';'
+      },
+      lib: {
+        src: [
+          'node_modules/jquery/dist/jquery.min.js',
+          'node_modules/underscore/underscore-min.js',
+          'node_modules/backbone/backbone-min.js'
+        ],
+        dest: 'app/assets/scripts/lib.js'
+      },
+      app: {
+        src: [
+          'app/src/javascript/alert.js'
+        ],
+        dest: 'app/assets/scripts/alert.js'
+      }
+    },
+
+    less: {
+      compile: {
+        options: {
+          expand: true,
+          flatten: true,
+          compress: true,
+          strictImports: true,
+          yuicompress: true,
+          optimization: 2,
+          // cwd: 'app/src/less',
+          // src: [
+            // 'test.less'
+          // ],
+          // dest: 'app/assets/stylesheets/test',
+          // ext: '.css',
+          // preExt: '.less',
+          // filter: 'isFile',
+        },
+        files: {
+          // target.css file: source.less file
+          "app/assets/stylsheets/test.css": "app/src/less/test.less"
+        }
+      }
+    },
 
   });
 
 	// Load tasks
 	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-less');
 
 	// Register tasks
   grunt.registerTask('default', [ 'connect', 'watch' ]);
+  grunt.registerTask('compile', [ 'concat:app', 'less'] );
+  grunt.registerTask('vendors', [ 'concat:lib'] );
+  // grunt.registerTask('less', [ 'less'] );
 
 };

e88263868b36c345b75bdbacfec4206702d2b40f
Frank Heiss <heissmusik@gmail.com>
2014-10-06 17:14:01 -0400

    a sequence model to hold steps collection, audio context and trigger oscillator

---
+++ app/src/js/models/sequence.js
@@ -0,0 +1,39 @@
+var Sequence = Backbone.Model.extend({
+
+	initialize: function(attrs) {
+		// console.log('Sequence::initialize()', attrs);
+		this.context = this.getAudioContext();
+		this.get('stepCollection').fetch().done( function() {
+		});
+	},
+
+	getAudioContext: function() {
+    var contextClass = ( window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.oAudioContext || window.msAudioContext);
+    if (contextClass) { // Web Audio API is available.
+      return new contextClass();
+    } else {
+      alert('Web Audio API not supported by this browser.  :-(   ');
+    }
+  },
+
+  createAndTriggerOscillator: function (freq, volume) {
+    var vco = this.context.createOscillator();
+    vco.frequency.value = freq;
+    vco.type = "square";
+    vco.connect(this.context.destination);
+
+    var vca = this.context.createGain();
+    var volume = Math.random();
+    vca.gain.value = volume;
+    vco.connect(vca);
+    vca.connect(this.context.destination);
+
+    vco.start(0);
+    var self= this;
+    setTimeout(function() {
+        vco.stop(0);
+        vco.disconnect(self.context.destination);
+    }, 100)
+  }
+
+});

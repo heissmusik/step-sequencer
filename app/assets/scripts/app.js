315cf536d96de8877f6997e3f4a2ba421180d588
Frank Heiss <heissmusik@gmail.com>
2014-10-12 20:21:19 -0400

    concat again

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -100,30 +100,13 @@ var Clock = Backbone.Model.extend({
 	initialize: function () {
 		console.log('FaderView::initialize()');
 		
-		// this.$el.draggable({ axis: "y" });
-		// var axis = $( ".selector" ).draggable( "option", "axis" );
-		// console.log(axis);
-
 		// this.$el.drags();
-
-		// $(this.el).bind("dragStart",
-		// 	function() {
-		// 		window.dragging = this.model;
-		// 	}, this);
-
-		//remove reference for garbage collection purpose
-		// $(this.el).bind("dragStop",
-		// 	function() {
-		// 		delete window.dragging;
-		// 	}, this);
 
 	},
 
 	dropviewDropHandler: function() { console.log('drag'); },
 
   render: function() {
-  	// $('.fader-view').append(stepView.render().$el);
-  	// this.template = _.template( $('#step-template').html() );
 		this.$el.html(this.template({
       id: this.model.id
 		}));
